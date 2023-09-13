a7e6ae9de43f75358468ab2647caa6018111d292
Norel <norel.evoagile@gmail.com>
2023-09-13 17:19:56 +0900

    Remove usued codes

--- config.h
+++ config.h
@@ -250,10 +250,6 @@ static MouseShortcut mshortcuts[] = {
 #define ALT_SHIFT (ALT|SHIFT)
 #define CTRL_SHIFT (CTRL|SHIFT)
 
-static char *openurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -o", "externalpipe", NULL };
-static char *copyurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -c", "externalpipe", NULL };
-static char *copyoutput[] = { "/bin/sh", "-c", "st-copyout", "externalpipe", NULL };
-
 static Shortcut shortcuts[] = {
 	/* mask           keysym          function        argument */
 	{ XK_ANY_MOD,     XK_Break,       sendbreak,      {.i =  0} },
